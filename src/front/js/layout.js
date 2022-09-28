5686e24af7f911ec814ace8bb97b340acd1d9d18
CristiianGB <cristian.guardenyo@gmail.com>
2022-09-28 18:23:27 +0000

    todo listo para empezar

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -21,12 +21,12 @@ const Layout = () => {
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
-                    <Routes>
+                    {/* <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
-                    </Routes>
+                    </Routes> */}
                     <Footer />
                 </ScrollToTop>
             </BrowserRouter>
--- src/front/js/pages/landingPage.jsx
+++ src/front/js/pages/landingPage.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
-import { Context } from "../store/appContext";
+import { Context } from "../store/appContext.jsx";
 
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
+		
 		<div className="container">
 			<ul className="list-group">
 				{store.demo.map((item, index) => {
