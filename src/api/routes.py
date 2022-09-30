c26cc52613007ea3274effe2757dda21b7b5e092
Kaiki <alejandrobisuteria@gmail.com>
2022-09-30 18:02:48 +0000

    Cristian puto

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Contact, Group
+from api.models import db, User, Favorites, Pokemon, Pokemon_Fusion
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt_identity
 
