2a921f4f751db5c6e922c8fc3c5ce9222551ecec
nanher94 <hernan.albiol@gmail.com>
2022-11-10 20:51:51 +0000

    Perfil actualizado

--- src/front/js/pages/pokeducatorPerfil.jsx
+++ src/front/js/pages/pokeducatorPerfil.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 const PokeducatorPerfil = () => {
   const { store, actions } = useContext(Context);
   const [user, setUser] = useState({});
+
   let navigate = useNavigate();
 
   useEffect(() => {
@@ -19,17 +20,21 @@ const PokeducatorPerfil = () => {
 
   useEffect(() => {
     setUser(store.user);
+    setPicProfile(store?.user?.img);
   }, [store.user]);
 
+  const [picProfile, setPicProfile] = useState("");
+
   const handleSubmit = async () => {
-    let url = document.getElementById("modalFoto");
-    fetch(
-      "https://3001-cristiiangb-pokeducator-b599gavfzv3.ws-eu75.gitpod.io/api/updateProfilePicture",
+    // let url = document.getElementById("modalFoto");
+    let url = picProfile;
+    const resp = await fetch(
+      "https://3001-cristiiangb-pokeducator-2oc1ugj4apt.ws-eu75.gitpod.io/api/updateProfilePicture",
       {
-        method: "POST",
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
-          Authorization: "Bearer " + token,
+          Authorization: "Bearer " + store.token,
         },
         body: JSON.stringify({
           img: url,
@@ -53,223 +58,208 @@ const PokeducatorPerfil = () => {
 
   const iconoTrue = "icono fas fa-check-circle";
   const iconoFalse = "icono far fa-check-circle";
-  
-  return (
-    <section className="seccion-perfil-usuario ">
-      <section style={{ backgroundColor: "#eee" }}>
-        <div className="container py-5">
-          <div className="row">
-            <div className="col">
-              <nav
-                aria-label="breadcrumb"
-                className="bg-light rounded-3 p-3 mb-4"
-              >
-                <ol className="breadcrumb mb-0">
-                  <li className="breadcrumb-item">
-                    <a href="#">Home</a>
-                  </li>
-                  <li className="breadcrumb-item">
-                    <a href="#">User</a>
-                  </li>
-                  <li className="breadcrumb-item active" aria-current="page">
-                    User Profile
-                  </li>
-                </ol>
-              </nav>
-            </div>
-          </div>
 
-          <div className="row">
-            <div className="col-lg-4">
-              <div className="card mb-4">
-                <div className="card-body text-center">
+  return (
+    <section style={{ backgroundColor: "#eee" }}>
+      <div className="container py-3">
+        <div className="row">
+          <div className="col">
+            <nav
+              aria-label="breadcrumb"
+              className="bg-light rounded-3 p-3 mb-4 text-center text-muted"
+            >
+              <h4>Perfil de Entrenador</h4>
+            </nav>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-4">
+            <div className="card">
+              <div className="card-body text-center">
+                <div className="imagen_perfil">
                   <img
-                    src={user?.img}
+                    src="https://upload.wikimedia.org/wikipedia/commons/8/87/Avatar_poe84it.png"
+                    // src={picProfile}
                     alt="avatar"
                     className="rounded-circle img-fluid"
                     style={{ width: "150px" }}
                   />
-                  <h5 className="my-3">{user?.username}</h5>
-                  <p className="text-muted mb-1">
-                    {user?.first_name + " " + user?.last_name}
-                  </p>
-                  <p className="text-muted mb-4">{user?.email}</p>
+                  {/* <!-- Button trigger modal --> */}
+                  <button
+                    type="button"
+                    className="btn btn-primary btn_imagen"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                  >
+                    <i className="fas fa-user-edit"></i>
+                  </button>
+
+                  {/* <!-- Modal --> */}
+                  <div
+                    className="modal fade"
+                    id="exampleModal"
+                    tabIndex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog">
+                      <div className="modal-content">
+                        <div className="modal-header">
+                          <h5 className="modal-title" id="exampleModalLabel">
+                            Cambiar imagen de perfil
+                          </h5>
+                        </div>
+                        <div className="modal-body">
+                          <div className="input-group mb-3">
+                            <span
+                              className="input-group-text"
+                              id="inputGroup-sizing-default"
+                            >
+                              Link Imagen
+                            </span>
+                            <input
+                              type="text"
+                              value={picProfile}
+                              className="form-control modalFoto"
+                              aria-label="Sizing example input"
+                              aria-describedby="inputGroup-sizing-default"
+                              onChange={(e) => setPicProfile(e.target.value)}
+                            />
+                          </div>
+                        </div>
+                        <div className="modal-footer">
+                          <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                          >
+                            Cerrar
+                          </button>
+                          <button
+                            type="submit"
+                            className="btn btn-primary"
+                            onClick={handleSubmit}
+                          >
+                            Guardar
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <h5 className="my-3">{"@" + user?.username}</h5>
+                <p className="text-muted mb-1 text-capitalize">
+                  {user?.first_name + " " + user?.last_name}
+                </p>
+                <p className="text-muted mb-4">{user?.email}</p>
+              </div>
+            </div>
+          </div>
+
+          {/* DATOS PERFIL */}
+          <div className="col-lg-8">
+            <div className="card mb-4">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-sm-3 fw-bold">
+                    <p className="mb-0">Usuario</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">{user?.username}</p>
+                  </div>
+                </div>
+                <hr />
+
+                <div className="row">
+                  <div className="col-sm-3 fw-bold">
+                    <p className="mb-0">Nombre</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">{user?.first_name}</p>
+                  </div>
+                </div>
+                <hr />
+
+                <div className="row">
+                  <div className="col-sm-3 fw-bold">
+                    <p className="mb-0">Apellido</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">{user?.last_name}</p>
+                  </div>
+                </div>
+                <hr />
+
+                <div className="row">
+                  <div className="col-sm-3 fw-bold">
+                    <p className="mb-0">Correo Electronico</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">{user?.email}</p>
+                  </div>
+                </div>
+                <hr />
+
+                <div className="row">
+                  <div className="col-sm-3 fw-bold">
+                    <p className="mb-0">Contraseña</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">**********</p>
+                  </div>
+                </div>
+                <hr />
+
+                <div className="row">
+                  <div className="col-sm-3 fw-bold">
+                    <p className="mb-0">Premium</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">
+                      <i
+                        className={user?.is_active ? iconoTrue : iconoFalse}
+                      ></i>
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className="card mb-4 mb-lg-0">
-                <div className="card-body p-0">
-                  <ul className="list-group list-group-flush rounded-3">
-                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                      <i className="fas fa-globe fa-lg text-warning"></i>
-                      <p className="mb-0">https://mdbootstrap.com</p>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                      <i
-                        className="fab fa-github fa-lg"
-                        style={{ color: "#333333" }}
-                      ></i>
-                      <p className="mb-0">mdbootstrap</p>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                      <i
-                        className="fab fa-twitter fa-lg"
-                        style={{ color: "#55acee" }}
-                      ></i>
-                      <p className="mb-0">@mdbootstrap</p>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                      <i
-                        className="fab fa-instagram fa-lg"
-                        style={{ color: "#ac2bac" }}
-                      ></i>
-                      <p className="mb-0">mdbootstrap</p>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                      <i
-                        className="fab fa-facebook-f fa-lg"
-                        style={{ color: "#3b5998" }}
-                      ></i>
-                      <p className="mb-0">mdbootstrap</p>
-                    </li>
-                  </ul>
+            </div>
+          </div>
+          <div className="container">
+            {/* EQUIPOS */}
+            <div className="row my-2">
+              <div className="col">
+                <div className="card mb-4 mb-md-0 p-3">
+                  <h4>Mis Equipos</h4>
+                  <div className="d-flex justify-content-around text-center">
+                    <div className="col-2">1</div>
+                    <div className="col-2">2</div>
+                    <div className="col-2">3</div>
+                    <div className="col-2">4</div>
+                    <div className="col-2">5</div>
+                    <div className="col-2">6</div>
+                  </div>
                 </div>
               </div>
             </div>
 
-            {/* DATOS PERFIL */}
-            <div className="col-lg-8">
-              <div className="card mb-4">
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-sm-3 fw-bold">
-                      <p className="mb-0">Usuario</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0">{user?.username}</p>
-                    </div>
-                  </div>
-                  <hr />
-
-                  <div className="row">
-                    <div className="col-sm-3 fw-bold">
-                      <p className="mb-0">Nombre</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0">
-                        {user?.first_name}
-                      </p>
-                    </div>
-                  </div>
-                  <hr />
-
-                  <div className="row">
-                    <div className="col-sm-3 fw-bold">
-                      <p className="mb-0">Apellido</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0">
-                        {user?.last_name}
-                      </p>
-                    </div>
-                  </div>
-                  <hr />
-
-                  <div className="row">
-                    <div className="col-sm-3 fw-bold">
-                      <p className="mb-0">Correo Electronico</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0">{user?.email}</p>
-                    </div>
-                  </div>
-                  <hr />
-
-                  <div className="row">
-                    <div className="col-sm-3 fw-bold">
-                      <p className="mb-0">Contraseña</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0">*********</p>
-                    </div>
-                  </div>
-                  <hr />
-
-                  <div className="row">
-                    <div className="col-sm-3 fw-bold">
-                      <p className="mb-0">Premium</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0">
-                        <i
-                          className={
-                            user?.is_active ? iconoTrue : iconoFalse
-                          }
-                        ></i>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              {/* EQUIPOS */}
-              <div className="row">
-                <div className="col">
-                  <div className="card mb-4 mb-md-0">
-                    <h4>Mis Equipos</h4>
-                    <p>ACA VAN LOS EQUIPOS</p>
-                  </div>
-                </div>
-              </div>
-
-              {/* VOTACIONES */}
-              <div className="row">
-                <div className="col">
-                  <div className="card mb-4 mb-md-0">
-                    <h4>Ultimas votaciones</h4>
-                    <p>Aca van las votaciones</p>
+            {/* VOTACIONES */}
+            <div className="row my-2">
+              <div className="col">
+                <div className="card mb-4 mb-md-0 p-3">
+                  <h4>Ultimas votaciones</h4>
+                  <div className="d-flex justify-content-around">
+                    <div className="col-3 bg-info">1</div>
+                    <div className="col-3 bg-info">2</div>
+                    <div className="col-3 bg-info">3</div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-      {/* <div classNameName="perfil-usuario-header">
-            <div classNameName="perfil-usuario-portada">
-                <div classNameName="perfil-usuario-avatar">
-                    <img src="https://i.pinimg.com/550x/f7/da/98/f7da9864a7c3079df7c26173520d18fc.jpg" alt="img-avatar"/>
-                    <button type="button" classNameName="boton-avatar">
-                        <i classNameName="far fa-image"></i>
-                    </button>
-                </div>
-                <a href="/login" classNameName="boton-portada"  onClick={logOut()}>
-                    <i classNameName="fas fa-sign-out-alt"></i>Cerrar sesión
-                </a>
-            </div>
-        </div>
-        <div classNameName="perfil-usuario-body">
-            <div classNameName="perfil-usuario-bio">
-                <h3 classNameName="titulo"></h3>
-                <p classNameName="texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-            <div classNameName="perfil-usuario-footer">
-                <ul classNameName="lista-datos">
-                    <li><i classNameName="icono fas fa-user"></i>{user?.username}</li>
-                    <li><i classNameName="icono fas fa-envelope"></i>{user?.email}</li>
-                </ul>
-				
-                <ul classNameName="lista-datos">
-                    <li><i classNameName="icono fas fa-key"></i>Contraseña: <a href="#">mostrar</a> </li>
-                    <li><i classNameName="icono fas fa-user-check"></i>Activo:<i classNameName={user?.is_active?iconoTrue:iconoFalse}></i></li>
-                </ul>
-            </div>
-            <div classNameName="redes-sociales">
-                <a href="" classNameName="boton-redes facebook fab fa-facebook-f"><i classNameName="icon-facebook"></i></a>
-                <a href="" classNameName="boton-redes twitter fab fa-twitter"><i classNameName="icon-twitter"></i></a>
-                <a href="" classNameName="boton-redes instagram fab fa-instagram"><i classNameName="icon-instagram"></i></a>
-            </div>
-        </div> */}
+      </div>
     </section>
   );
 };
--- src/front/js/pages/pokeducatorSignup.jsx
+++ src/front/js/pages/pokeducatorSignup.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Context } from "../store/appContext.jsx";
-import "../../styles/pokeducatorSignupStyles.css"
+import "../../styles/pokeducatorSignupStyles.css";
 
 const PokeducatorSignup = (props) => {
   const { store, actions } = useContext(Context);
@@ -15,17 +15,20 @@ const PokeducatorSignup = (props) => {
   let navigate = useNavigate();
   // hacer signup en actions y llamar a actions desde aqui
   const handleSignup = async () => {
-    const resp = await fetch("https://3001-cristiiangb-pokeducator-b599gavfzv3.ws-eu75.gitpod.io/api/signup", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        username: username,
-        email: email,
-        first_name: first_name,
-        last_name: last_name,
-        password: password,
-      }),
-    });
+    const resp = await fetch(
+      "https://3001-cristiiangb-pokeducator-2oc1ugj4apt.ws-eu74.gitpod.io/api/signup",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          first_name: first_name,
+          last_name: last_name,
+          password: password,
+        }),
+      }
+    );
 
     if (!resp.ok) throw Error("There was a problem in the signup request");
 
@@ -39,81 +42,83 @@ const PokeducatorSignup = (props) => {
     //also you should set your user into the store using the setStore function
 
     // localStorage.setItem("jwt-token", data.token);
-    navigate("/perfil")
+    navigate("/perfil");
   };
 
   return (
     <div className="divSignup">
-        <section className="form-register">
-      <h4>Formulario Registro</h4>
-      <input
-        className="controls"
-        type="text"
-        name="usuario"
-        id="usuario"
-        placeholder="Elige nombre de Usuario"
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
-      <input
-        className="controls"
-        type="text"
-        name="first_name"
-        id="first_name"
-        placeholder="Ingresa tu Nombre"
-        onChange={(e) => {
-          setFirstName(e.target.value);
-        }}
-      />
-      <input
-        className="controls"
-        type="text"
-        name="last_name"
-        id="last_name"
-        placeholder="Ingresa tu Apellido"
-        onChange={(e) => {
-          setLastName(e.target.value);
-        }}
-      />
-      <input
-        className="controls"
-        type="email"
-        name="correo"
-        id="correo"
-        placeholder="No olvides el Correo electrónico"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <input
-        className="controls"
-        type="password"
-        name="contraseña"
-        id="password"
-        placeholder="Por último una Contraseña"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <p className="separadorBottom terminosYCondiciones">
-        Al registrarse validará <a href="https://www.laps4.com/comunidad/threads/los-10-troll-mandamientos.343028/">Terminos y Condiciones</a>
-      </p>
-      <button
-        className="botons buttonPokemonInfo"
-        onClick={() => {
-          handleSignup();
-        }}
-        value="Registrar"
-      >
-        Registrar
-      </button>
-      <p>
-        <Link to="/login">Ya tengo cuenta.</Link>
-      </p>
-    </section>
+      <section className="form-register">
+        <h4>Formulario Registro</h4>
+        <input
+          className="controls"
+          type="text"
+          name="usuario"
+          id="usuario"
+          placeholder="Elige nombre de Usuario"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="text"
+          name="first_name"
+          id="first_name"
+          placeholder="Ingresa tu Nombre"
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="text"
+          name="last_name"
+          id="last_name"
+          placeholder="Ingresa tu Apellido"
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="email"
+          name="correo"
+          id="correo"
+          placeholder="No olvides el Correo electrónico"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="password"
+          name="contraseña"
+          id="password"
+          placeholder="Por último una Contraseña"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <p className="separadorBottom terminosYCondiciones">
+          Al registrarse validará{" "}
+          <a href="https://www.laps4.com/comunidad/threads/los-10-troll-mandamientos.343028/">
+            Terminos y Condiciones
+          </a>
+        </p>
+        <button
+          className="botons buttonPokemonInfo"
+          onClick={() => {
+            handleSignup();
+          }}
+          value="Registrar"
+        >
+          Registrar
+        </button>
+        <p>
+          <Link to="/login">Ya tengo cuenta.</Link>
+        </p>
+      </section>
     </div>
-    
   );
 };
 
